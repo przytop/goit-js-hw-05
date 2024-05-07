fa5fc4b3d61cb372b1ca4b55720f3c539b11eeca
Paweł <102540215+przytop@users.noreply.github.com>
2024-05-07 22:37:37 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function isEnoughCapacity(products, containerSize) {
+  let totalSize = 0;
+  for (let size of Object.values(products)) {
+    totalSize += size;
+  }
+
+  return totalSize <= containerSize ? true : false;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
